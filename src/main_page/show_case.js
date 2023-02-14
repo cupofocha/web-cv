a1dd97a65e05d0dbbb2b62c437506a63f730c559
cupofocha <ochatastesgood@gmail.com>
2023-02-14 10:49:26 +0100

    project showcase smooth scrolling and auto scrolling implemented

--- src/main_page/show_case.js
+++ src/main_page/show_case.js
@@ -1,7 +1,33 @@
 import "./css/show_case.css"
 import logo from "../logo.svg";
+import ScrollIntoView from 'react-scroll-into-view'
+import {useEffect, useRef, useState} from "react";
 
 export default function ShowCase() {
+    const [counter, setCounter] = useState(5)
+    const [currentPage, setCurrentPage] = useState(1)
+
+    const project_2 = useRef(null)
+    const project_5 = useRef(null)
+
+    useEffect(() => {
+        if (counter > 0)
+            setTimeout(() => setCounter(counter - 1), 1000)
+        else {
+            console.log("Time out2!")
+            if (currentPage === 1) {
+                project_5.current.scrollIntoView({behavior: "smooth", block: "nearest", inline: "center"})
+                setCurrentPage(2)
+                setCounter(8)
+            }
+            else {
+                project_2.current.scrollIntoView({behavior: "smooth", block: "nearest", inline: "center"})
+                setCurrentPage(1)
+                setCounter(8)
+            }
+        }
+    }, [counter]);
+
     return (
         <div id="show_cases_container">
             <h1>Project Show Case</h1>
@@ -17,7 +43,7 @@ export default function ShowCase() {
                         <h4 id="project_1_tool">Tool: React, Spring Boot</h4>
                     </div>
                 </div>
-                <div id="project_2" className="project">
+                <div id="project_2" className="project" ref={project_2}>
                     <img id="project_2_screen_shot"
                          src={logo}
                          className="project_screen_shot"
@@ -50,8 +76,8 @@ export default function ShowCase() {
                         <h4 id="project_4_tool">Tool: React, Spring Boot</h4>
                     </div>
                 </div>
-                <div id="project_5" className="project">
-                                            <img id="project_5_screen_shot"
+                <div id="project_5" className="project" ref={project_5}>
+                    <img id="project_5_screen_shot"
                          src={logo}
                          className="project_screen_shot"
                     />
@@ -72,6 +98,24 @@ export default function ShowCase() {
                         <h4 id="project_6_tool">Tool: React, Spring Boot</h4>
                     </div>
                 </div>
+            </div>
+            <div id="page_hrefs">
+                <ScrollIntoView id="page_href_1" selector="#project_2" scrollOptions={{inline: "center", block: "nearest"}}
+                                onClick={()=>{
+                                    setCurrentPage(2)
+                                }}>
+                    <div id="page_1" className="page_href">
+                        <img src={logo}/>
+                    </div>
+                </ScrollIntoView>
+                <ScrollIntoView id="page_href_2" selector="#project_5" scrollOptions={{inline: "center", block: "nearest"}}
+                                onClick={()=>{
+                                    setCurrentPage(1)
+                                }}>
+                    <div id="page_2" className="page_href">
+                        <img src={logo}/>
+                    </div>
+                </ScrollIntoView>
             </div>
         </div>
 
